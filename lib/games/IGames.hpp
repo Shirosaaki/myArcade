fe0750aefb0601a0e1f36992299b3e5ec0602917
shirosaaki <matheo.campanella@epitech.eu>
2025-03-18 13:01:06 +0100

    [+] Interface component

---
+++ lib/games/IGames.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2025
+** IGames.hpp
+** File description:
+** IGames.hpp
+*/
+#ifndef IGAMES_HPP_
+    #define IGAMES_HPP_
+    #include <iostream>
+    #include <vector>
+    #include <map>
+    #include <string>
+
+namespace arcade {
+    
+    class IGames {
+        public:
+            virtual ~IGames() = default;
+            virtual std::map<std::string, std::vector<std::pair<int, int>>> GetDisplay(enum TGraphics lib) = 0;
+            virtual void setKey(enum KeyBind key) = 0;
+            virtual int getScore() = 0;
+            virtual std::string getSound(enum TGraphics lib) = 0;
+
+        private:
+    };
+}
+
+#endif /* !IGAMES_HPP_ */
---
+++ lib/graphicals/IGraphics.hpp
@@ -0,0 +1,71 @@
+/*
+** EPITECH PROJECT, 2025
+** IGraphics
+** File description:
+** IGraphics
+*/
+#ifndef IGRAPHICS_HPP_
+    #define IGRAPHICS_HPP_
+    #include <iostream>
+    #include <vector>
+    #include <string>
+    #include <map>
+
+namespace arcade {
+
+    enum class TGraphics {
+        NCURSES,
+        SDL,
+        NDK,
+        AA,
+        CACA,
+        ALLEGRO,
+        X,
+        GTK,
+        SFML,
+        IRRLICHT,
+        OPENGL,
+        VULKAN,
+        QT
+    };
+
+    enum class TGames {
+        SNAKE,
+        MINESWEEPER,
+        NIBBLER,
+        PACMAN,
+        QIX,
+        CENTIPEDE,
+        SOLARFOX
+    };
+
+    enum class KeyBind {
+        ESC,
+        A_KEY,
+        Z_KEY,
+        UP_KEY,
+        DOWN_KEY,
+        LEFT_KEY,
+        RIGHT_KEY,
+        SPACE,
+        ENTER,
+        MOUSE_LEFT,
+        MOUSE_RIGHT,
+        NONE
+    };
+
+    class IGraphics {
+        public:
+            virtual ~IGraphics() = default;
+            virtual void Init() = 0;
+            virtual KeyBind getKey() = 0;
+            virtual void Display(std::map<std::string, std::vector<std::pair<int, int>>> &entities) = 0;
+            virtual void PlaySound(std::string sound) = 0;
+            virtual void Clear() = 0;
+            virtual void Nuke() = 0;
+
+        private:
+    };
+}
+
+#endif /* !IGRAPHICS_HPP_ */
