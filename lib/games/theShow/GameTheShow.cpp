1fcfac0d7bc22f83517c6caa24b18bffd1747112
shirosaaki <matheo.campanella@epitech.eu>
2025-04-04 17:05:09 +0200

    [+] level2 the show

--- lib/games/theShow/GameTheShow.cpp
+++ lib/games/theShow/GameTheShow.cpp
@@ -8,7 +8,8 @@
 
 arcade::GameTheShow::GameTheShow()
 {
-    this->pos = std::make_pair(0, 360);
+    this->pos = std::make_pair(630, 360);
+    this->ennemiPos = std::make_pair(800, 300);
 }
 
 arcade::GameTheShow::~GameTheShow()
@@ -37,21 +38,19 @@ std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int,
     return entities;
 }
 
-std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::GetDisplay(enum TGraphics lib)
+std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::displaySecondCinematic()
 {
-    if (lib == TGraphics::NCURSES) {
-        std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>> entity;
-        entity.first = "THE_SHOW";
-        entity.second.first = std::make_pair(0, 0);
-        entity.second.second = std::make_pair(6, 6);
-        return {entity};
-    } if (isTheFirstTime == true)
-        return displayFirstCinematic();
+    std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> entities;
+    return entities;
+}
+
+std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::displayLevel1()
+{
+    std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> entities;
     auto now = std::chrono::steady_clock::now();
     auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - timeMove);
     if (elapsed.count() > 400)
         isMoving = false;
-    std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> entities;
     entities.emplace_back("assets/theShow/maison.png", std::make_pair(std::make_pair(0, 0), std::make_pair(1080, 720)));
     if (isMoving == true) {
         elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastMoveTime);
@@ -81,27 +80,213 @@ std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int,
     return entities;
 }
 
+std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::displayLevel2()
+{
+    std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> entities;
+    auto now = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - timeMove);
+    if (elapsed.count() > 400)
+        isMoving = false;
+    entities.emplace_back("assets/theShow/ada.png", std::make_pair(std::make_pair(0, 0), std::make_pair(1080, 720)));
+    if (isMoving == true) {
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastMoveTime);
+        if (elapsed.count() > 200) {
+            moveIndex = (moveIndex + 1) % 3;
+            lastMoveTime = now;
+        }
+        if (isRight == true) {
+            if (moveIndex == 0)
+                entities.emplace_back("assets/theShow/EricIdle.png", std::make_pair(pos, std::make_pair(90, 150)));
+            else if (moveIndex == 1)
+                entities.emplace_back("assets/theShow/EricWalk1.png", std::make_pair(pos, std::make_pair(100, 150)));
+            else if (moveIndex == 2)
+                entities.emplace_back("assets/theShow/EricWalk2.png", std::make_pair(pos, std::make_pair(105, 150)));
+        } else {
+            if (moveIndex == 0)
+                entities.emplace_back("assets/theShow/EricIdle2.png", std::make_pair(pos, std::make_pair(90, 150)));
+            else if (moveIndex == 1)
+                entities.emplace_back("assets/theShow/EricWalk12.png", std::make_pair(pos, std::make_pair(100, 150)));
+            else if (moveIndex == 2)
+                entities.emplace_back("assets/theShow/EricWalk22.png", std::make_pair(pos, std::make_pair(105, 150)));
+        }
+    } else if (isAttack == true) {
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastMoveTime);
+        if (elapsed.count() > 200) {
+            moveIndex = (moveIndex + 1) % 5;
+            lastMoveTime = now;
+            if (moveIndex == 4) {
+                isAttack = false;
+                moveIndex = 0;
+                ennemiLife--;
+                if (ennemiLife == 0) {
+                    actualLevel = 1;
+                    isMoving = false;
+                    isRight = true;
+                    pos.first = 630;
+                    pos.second = 360;
+                    canAttack = true;
+                    ennemiLife = 3;
+                    life = 3;
+                    _needDisplaySecondCinematic = true;
+                }
+            }
+        }
+        if (isRight == true) {
+            if (moveIndex == 0)
+                entities.emplace_back("assets/theShow/EricIdle.png", std::make_pair(pos, std::make_pair(90, 150)));
+            else if (moveIndex == 1)
+                entities.emplace_back("assets/theShow/EricAttack1.png", std::make_pair(pos, std::make_pair(100, 150)));
+            else if (moveIndex == 2)
+                entities.emplace_back("assets/theShow/EricAttack2.png", std::make_pair(pos, std::make_pair(105, 150)));
+            else if (moveIndex == 3)
+                entities.emplace_back("assets/theShow/EricAttack3.png", std::make_pair(pos, std::make_pair(105, 150)));
+        } else {
+            if (moveIndex == 0)
+                entities.emplace_back("assets/theShow/EricIdle2.png", std::make_pair(pos, std::make_pair(90, 150)));
+            else if (moveIndex == 1)
+                entities.emplace_back("assets/theShow/EricAttack12.png", std::make_pair(pos, std::make_pair(100, 150)));
+            else if (moveIndex == 2)
+                entities.emplace_back("assets/theShow/EricAttack22.png", std::make_pair(pos, std::make_pair(105, 150)));
+            else if (moveIndex == 3)
+                entities.emplace_back("assets/theShow/EricAttack32.png", std::make_pair(pos, std::make_pair(105, 150)));
+        }
+    }  else if (isRight == true)
+        entities.emplace_back("assets/theShow/EricIdle.png", std::make_pair(pos, std::make_pair(90, 150)));
+    else
+        entities.emplace_back("assets/theShow/EricIdle2.png", std::make_pair(pos, std::make_pair(90, 150)));
+    if (isEnnemiAttack == true) {
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastEnnemiMoveTime);
+        if (elapsed.count() > 200) {
+            ennemiMoveIndex = (ennemiMoveIndex + 1) % 5;
+            lastEnnemiMoveTime = now;
+            if (ennemiMoveIndex == 4) {
+                isEnnemiAttack = false;
+                ennemiMoveIndex = 0;
+                life--;
+                if (life == 0) {
+                    actualLevel = 3;
+                    isMoving = false;
+                    isRight = true;
+                    pos.first = 630;
+                    pos.second = 360;
+                    canAttack = false;
+                    ennemiLife = 3;
+                    life = 3;
+                }
+            }
+        }
+        if (isEnnemiRight == true) {
+            if (ennemiMoveIndex == 0)
+                entities.emplace_back("assets/theShow/PaulIdle.png", std::make_pair(ennemiPos, std::make_pair(90, 150)));
+            else if (ennemiMoveIndex == 1)
+                entities.emplace_back("assets/theShow/PaulAttack1.png", std::make_pair(ennemiPos, std::make_pair(100, 150)));
+            else if (ennemiMoveIndex == 2)
+                entities.emplace_back("assets/theShow/PaulAttack2.png", std::make_pair(ennemiPos, std::make_pair(105, 150)));
+            else if (ennemiMoveIndex == 3)
+                entities.emplace_back("assets/theShow/PaulAttack3.png", std::make_pair(ennemiPos, std::make_pair(105, 150)));
+        } else {
+            if (ennemiMoveIndex == 0)
+                entities.emplace_back("assets/theShow/PaulIdle2.png", std::make_pair(ennemiPos, std::make_pair(90, 150)));
+            else if (ennemiMoveIndex == 1)
+                entities.emplace_back("assets/theShow/PaulAttack12.png", std::make_pair(ennemiPos, std::make_pair(100, 150)));
+            else if (ennemiMoveIndex == 2)
+                entities.emplace_back("assets/theShow/PaulAttack22.png", std::make_pair(ennemiPos, std::make_pair(105, 150)));
+            else if (ennemiMoveIndex == 3)
+                entities.emplace_back("assets/theShow/PaulAttack32.png", std::make_pair(ennemiPos, std::make_pair(105, 150)));
+        }
+    } else if (isEnnemiRight == true)
+        entities.emplace_back("assets/theShow/PaulIdle.png", std::make_pair(ennemiPos, std::make_pair(90, 150)));
+    else
+        entities.emplace_back("assets/theShow/PaulIdle2.png", std::make_pair(ennemiPos, std::make_pair(90, 150)));
+    return entities;
+}
+
+std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::displayLevel3()
+{
+    std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> entities;
+    return entities;
+}
+
+std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::displayLevel4()
+{
+    std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> entities;
+    return entities;
+}
+
+std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::displayLevel5()
+{
+    std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> entities;
+    return entities;
+}
+
+void arcade::GameTheShow::changeLevel()
+{
+    return;
+}
+
+std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::GetDisplay(enum TGraphics lib)
+{
+    if (lib == TGraphics::NCURSES) {
+        std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>> entity;
+        entity.first = "THE_SHOW";
+        entity.second.first = std::make_pair(0, 0);
+        entity.second.second = std::make_pair(6, 6);
+        return {entity};
+    } if (isTheFirstTime == true)
+        return displayFirstCinematic();
+    if (_needDisplaySecondCinematic == true) {
+        _needDisplaySecondCinematic = false;
+        return displaySecondCinematic();
+    }
+    if (actualLevel == 1)
+        return displayLevel1();
+    if (actualLevel == 2)
+        return displayLevel2();
+    if (actualLevel == 3)
+        return displayLevel3();
+    if (actualLevel == 4)
+        return displayLevel4();
+    else
+        return displayLevel5();
+}
+
 void arcade::GameTheShow::setKey(enum KeyBind key)
 {
     if (key == KeyBind::LEFT_KEY && canMove && pos.first > 0) {
         pos.first -= 10;
         isRight = false;
         isMoving = true;
+        isAttack = false;
         timeMove = std::chrono::steady_clock::now();
-        return;
-    }
-    if (key == KeyBind::RIGHT_KEY && canMove && pos.first < 990) {
+    } else if (key == KeyBind::RIGHT_KEY && canMove && pos.first < 990) {
         pos.first += 10;
         isRight = true;
         isMoving = true;
+        isAttack = false;
         timeMove = std::chrono::steady_clock::now();
-        return;
+    } else if (key == KeyBind::ENTER && canAttack == true) {
+        isAttack = true;
+        isMoving = false;
     }
+    if (pos.first == 0 && actualLevel == 1) {
+        actualLevel = 2;
+        isMoving = false;
+        isRight = true;
+        pos.first = 250;
+        pos.second = 300;
+        canAttack = true;
+    }
+    if (pos.first > ennemiPos.first + 20)
+        isEnnemiRight = true;
+    else if (pos.first < ennemiPos.first - 20)
+        isEnnemiRight = false;
+    if (pos.first > ennemiPos.first - 50 && pos.first < ennemiPos.first + 50 && isEnnemiAttack == false)
+        isEnnemiAttack = true;
 }
 
 int arcade::GameTheShow::getScore()
 {
-    return 1000000000;
+    return 1410065407;
 }
 
 std::string arcade::GameTheShow::getSound(enum TGraphics lib)
--- lib/games/theShow/GameTheShow.hpp
+++ lib/games/theShow/GameTheShow.hpp
@@ -23,20 +23,38 @@ namespace arcade {
             std::string getActGame();
             void resetGame() override { return; }
 
+        protected:
             std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> displayFirstCinematic();
+            std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> displaySecondCinematic();
+            std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> displayLevel1();
+            std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> displayLevel2();
+            std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> displayLevel3();
+            std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> displayLevel4();
+            std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> displayLevel5();
+            void changeLevel();
         
         private:
-            std::map<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>> entities;
             std::string actGame;
             bool canMove = true;
             bool isRight = true;
             bool isMoving = false;
             std::pair<int, int> pos;
             int moveIndex = 0;
-            bool isTheFirstTime = true;
+            bool isTheFirstTime = false;
+            bool canAttack = false;
+            bool isAttack = false;
             std::chrono::time_point<std::chrono::steady_clock> lastMoveTime = std::chrono::steady_clock::now();
             std::chrono::time_point<std::chrono::steady_clock> timeMove = std::chrono::steady_clock::now();
+            std::chrono::time_point<std::chrono::steady_clock> lastEnnemiMoveTime = std::chrono::steady_clock::now();
             int nbFrames = 0;
+            int actualLevel = 1;
+            int life = 3;
+            int ennemiLife = 3;
+            bool isEnnemiRight = false;
+            bool isEnnemiAttack = false;
+            std::pair<int, int> ennemiPos;
+            int ennemiMoveIndex = 0;
+            bool _needDisplaySecondCinematic = false;
     };
 }
 
