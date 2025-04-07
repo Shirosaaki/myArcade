3866c47a86e3f1fa2d6ede6fd84810a38c5312b3
shirosaaki <matheo.campanella@epitech.eu>
2025-04-07 16:22:14 +0200

    [+] big part of the third part of theShow

--- lib/games/theShow/GameTheShow.cpp
+++ lib/games/theShow/GameTheShow.cpp
@@ -220,6 +220,102 @@ std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int,
 std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> arcade::GameTheShow::displayLevel3()
 {
     std::vector<std::pair<std::string, std::pair<std::pair<int, int>, std::pair<int, int>>>> entities;
+    auto now = std::chrono::steady_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - timeMove);
+    if (elapsed.count() > 400)
+        isMoving = false;
+    entities.emplace_back("assets/theShow/phoenix.png", std::make_pair(std::make_pair(0, 0), std::make_pair(1080, 720)));
+    if (isMoving == true) {
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastMoveTime);
+        if (elapsed.count() > 200) {
+            moveIndex = (moveIndex + 1) % 3;
+            lastMoveTime = now;
+        }
+        if (isRight == true) {
+            if (moveIndex == 0)
+                entities.emplace_back("assets/theShow/EricIdle.png", std::make_pair(pos, std::make_pair(90, 150)));
+            else if (moveIndex == 1)
+                entities.emplace_back("assets/theShow/EricWalk1.png", std::make_pair(pos, std::make_pair(100, 150)));
+            else if (moveIndex == 2)
+                entities.emplace_back("assets/theShow/EricWalk2.png", std::make_pair(pos, std::make_pair(105, 150)));
+        } else {
+            if (moveIndex == 0)
+                entities.emplace_back("assets/theShow/EricIdle2.png", std::make_pair(pos, std::make_pair(90, 150)));
+            else if (moveIndex == 1)
+                entities.emplace_back("assets/theShow/EricWalk12.png", std::make_pair(pos, std::make_pair(100, 150)));
+            else if (moveIndex == 2)
+                entities.emplace_back("assets/theShow/EricWalk22.png", std::make_pair(pos, std::make_pair(105, 150)));
+        }
+    } else if (isRight == true)
+        entities.emplace_back("assets/theShow/EricIdle.png", std::make_pair(pos, std::make_pair(90, 150)));
+    else
+        entities.emplace_back("assets/theShow/EricIdle2.png", std::make_pair(pos, std::make_pair(90, 150)));
+    if (isEnnemiAttack == true) {
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastEnnemiMoveTime);
+        if (elapsed.count() > 200) {
+            ennemiMoveIndex = (ennemiMoveIndex + 1) % 4;
+            lastEnnemiMoveTime = now;
+            if (ennemiMoveIndex == 3) {
+                isEnnemiAttack = false;
+                ennemiMoveIndex = 0;
+                life--;
+                if (life == 0) {
+                    actualLevel = 3;
+                    isMoving = false;
+                    isRight = true;
+                    pos.first = 630;
+                    pos.second = 360;
+                    canAttack = false;
+                    ennemiLife = 3;
+                    life = 3;
+                }
+            }
+        }
+        if (isEnnemiRight == true) {
+            if (ennemiMoveIndex == 0)
+                entities.emplace_back("assets/theShow/SammyIdle.png", std::make_pair(ennemiPos, std::make_pair(90, 150)));
+            else if (ennemiMoveIndex == 1)
+                entities.emplace_back("assets/theShow/SammyAttack1.png", std::make_pair(ennemiPos, std::make_pair(100, 150)));
+            else if (ennemiMoveIndex == 2)
+                entities.emplace_back("assets/theShow/SammyAttack2.png", std::make_pair(ennemiPos, std::make_pair(105, 150)));
+        } else {
+            if (ennemiMoveIndex == 0)
+                entities.emplace_back("assets/theShow/SammyIdle2.png", std::make_pair(ennemiPos, std::make_pair(90, 150)));
+            else if (ennemiMoveIndex == 1)
+                entities.emplace_back("assets/theShow/SammyAttack12.png", std::make_pair(ennemiPos, std::make_pair(100, 150)));
+            else if (ennemiMoveIndex == 2)
+                entities.emplace_back("assets/theShow/SammyAttack22.png", std::make_pair(ennemiPos, std::make_pair(105, 150)));
+        }
+    } else if (isEnnemiRight == true)
+        entities.emplace_back("assets/theShow/SammyIdle.png", std::make_pair(ennemiPos, std::make_pair(90, 150)));
+    else
+        entities.emplace_back("assets/theShow/SammyIdle2.png", std::make_pair(ennemiPos, std::make_pair(90, 150)));
+    if (isAttack) {
+        elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(now - lastMoveTime);
+        if (elapsed.count() > 200) {
+            moveIndex = (moveIndex + 1) % 5;
+            lastMoveTime = now;
+        }
+        if (isRight) {
+            if (moveIndex == 0)
+                entities.emplace_back("assets/theShow/EricIdle.png", std::make_pair(pos, std::make_pair(90, 150)));
+            else if (moveIndex == 1)
+                entities.emplace_back("assets/theShow/EricAttack1.png", std::make_pair(pos, std::make_pair(100, 150)));
+            else if (moveIndex == 2)
+                entities.emplace_back("assets/theShow/EricAttack2.png", std::make_pair(pos, std::make_pair(105, 150)));
+            else if (moveIndex == 3)
+                entities.emplace_back("assets/theShow/EricAttack3.png", std::make_pair(pos, std::make_pair(105, 150)));
+        } else {
+            if (moveIndex == 0)
+                entities.emplace_back("assets/theShow/EricIdle2.png", std::make_pair(pos, std::make_pair(90, 150)));
+            else if (moveIndex == 1)
+                entities.emplace_back("assets/theShow/EricAttack12.png", std::make_pair(pos, std::make_pair(100, 150)));
+            else if (moveIndex == 2)
+                entities.emplace_back("assets/theShow/EricAttack22.png", std::make_pair(pos, std::make_pair(105, 150)));
+            else if (moveIndex == 3)
+                entities.emplace_back("assets/theShow/EricAttack32.png", std::make_pair(pos, std::make_pair(105, 150)));
+        }
+    }
     return entities;
 }
 
